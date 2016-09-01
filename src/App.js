ab675fdd1f486d12b151c08f14100a0e4dea8f23
Brandon Hilton <brandonhilton@Brandons-MacBook-Pro-2.local>
2016-09-01 15:12:58 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
@@ -13,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>something else</p>
+        <p>something more</p>
       </div>
     );
   }
